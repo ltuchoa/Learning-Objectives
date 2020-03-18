cb63c20aad0ca7f1421ee19ce94f363c3a1e7a7e
Larissa Uchôa <47635493+ltuchoa@users.noreply.github.com>
2020-03-17 21:06:38 -0300

    Create ConstantsVariables.swift
    Objectives 380, 381, 382, 383

---
+++ ConstantsVariables.swift
@@ -0,0 +1,18 @@
+// Objectives 380, 381, 382, 383
+
+// VARIABLES, CONSTANTS, COMMENTS, SEMI-COLONS
+
+/*Constant and variable names can’t contain whitespace characters, mathematical symbols, arrows, private-use Unicode scalar values, or line- and box-drawing characters. Nor can they begin with a number, although numbers may be included elsewhere within the name.
+*/
+//Once you’ve declared a constant or variable of a certain type, you can’t declare it again with the same name, or change it //to store values of a different type. Nor can you change a constant into a variable or a variable into a constant.
+
+var variavel: String
+let constante = 5
+
+variavel = "Pipoca"
+
+print(variavel)
+print(constante)
+
+// If I want to print in a single line
+print(variavel); print(constante)
