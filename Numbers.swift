a379d9b05236280b40a5706ba598d856012ecd5a
Larissa Uchôa <47635493+ltuchoa@users.noreply.github.com>
2020-03-17 21:09:10 -0300

    Create Numbers.swift
    Objectives 384, 385, 386, 387

---
+++ Numbers.swift
@@ -0,0 +1,18 @@
+// Objectives 384, 385, 386, 387
+
+// integers
+let x = 5
+let y = 7
+let soma = x + y
+
+// float or double
+let pi = 3.14159
+
+// numeric literals
+let decimal = 19
+let binary = 0b10011
+let hex = 0x13
+
+// type conversion
+
+let z = Int(pi)
