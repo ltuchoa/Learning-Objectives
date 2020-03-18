86e21d82c4fe9df36e60ba1c8e002ab6dd4e3daf
Larissa Uchôa <47635493+ltuchoa@users.noreply.github.com>
2020-03-17 22:03:29 -0300

    Create Ternary.swift
    Objective 331

---
+++ Ternary.swift
@@ -0,0 +1,11 @@
+// Objective 331
+
+let food = "Banana"
+var eating = true
+
+var action = eating ? food : "Not eating"
+print(action)
+
+eating = false
+action = eating ? food : "Not eating"
+print(action)
