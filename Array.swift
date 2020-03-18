44a60ee7dd0045b4505c2e568aa8469d30ccd096
Larissa Uchôa <47635493+ltuchoa@users.noreply.github.com>
2020-03-17 21:18:19 -0300

    Update Array.swift
    Objectives 343, 344, 345, 346 UPDATE

--- Array.swift
+++ Array.swift
@@ -5,11 +5,23 @@ var array = [Int]()
 
 // access and modify, iterate over an array
 var places = [String]()
+
 if places.isEmpty {print("Empty")} else {print(places)}
+
 places.append("Recife")
 places.append("Belém")
 places += ["Fortaleza", "Gramado"]
+
 var first = places[0]
 print(first)
+
 places[0] = "Guaramiranga"
 print(places)
+
+for place in places {
+  print("Place: \(place)")
+}
+
+for (i, place) in places.enumerated() {
+  print("Place \(i+1): \(place)")
+}
