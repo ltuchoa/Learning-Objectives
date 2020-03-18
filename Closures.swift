89b2071b8ca2c8c38c18b3f5800c073eefec3e09
Larissa Uchôa <47635493+ltuchoa@users.noreply.github.com>
2020-03-17 21:11:32 -0300

    incomplete
    Objectives 336 - 342 (INCOMPLETE)

---
+++ Closures.swift
@@ -0,0 +1,13 @@
+// Objectives 336 - 342 (INCOMPLETE)
+
+let eating = { (food: String) in
+  print("I'm eating \(food)")
+}
+
+eating("banana")
+
+let driving = { (place: String) in 
+  print("Driving to \($0)")
+}
+
+driving("Aldeota")
